cc83414c346a2b7c74ccd9ccc8c9097fcf5e23de
lithid <long.jeremie@gmail.com>
2012-09-05 10:37:10 -0400

    PowerWidget: Don't show mobile data options if not supported
    Change-Id: Ibb2624a4a657f7c5d203996c61afd0c6d82e5fe2

--- src/com/android/settings/cnd/PowerWidget.java
+++ src/com/android/settings/cnd/PowerWidget.java
@@ -161,6 +161,7 @@ public class PowerWidget extends SettingsPreferenceFragment implements
         private static final String TAG = "PowerWidgetActivity";
 
         private static final String BUTTONS_CATEGORY = "pref_buttons";
+        private static final String BUTTON_MODES_CATEGORY = "pref_buttons_modes";
         private static final String SELECT_BUTTON_KEY_PREFIX = "pref_button_";
 
         private static final String EXP_BRIGHTNESS_MODE = "pref_brightness_mode";
@@ -188,6 +189,7 @@ public class PowerWidget extends SettingsPreferenceFragment implements
             addPreferencesFromResource(R.xml.power_widget);
 
             PreferenceScreen prefSet = getPreferenceScreen();
+            PackageManager pm = getPackageManager();
 
             if (getActivity().getApplicationContext() == null) {
                 return;
@@ -232,6 +234,10 @@ public class PowerWidget extends SettingsPreferenceFragment implements
             PreferenceCategory prefButtons = (PreferenceCategory) prefSet
                     .findPreference(BUTTONS_CATEGORY);
 
+            // Add the available mode buttons, incase they need to be removed later
+            PreferenceCategory prefButtonsModes = (PreferenceCategory) prefSet
+                    .findPreference(BUTTON_MODES_CATEGORY);
+
             // empty our preference category and set it to order as added
             prefButtons.removeAll();
             prefButtons.setOrderingAsAdded(false);
@@ -247,6 +253,14 @@ public class PowerWidget extends SettingsPreferenceFragment implements
             boolean isWimaxEnabled = WimaxHelper.isWimaxSupported(getActivity());
             if (!isWimaxEnabled) {
                 PowerWidgetUtil.BUTTONS.remove(PowerWidgetUtil.BUTTON_WIMAX);
+            }
+
+            // Don't show mobile data options if not supported
+            boolean isMobileData = pm.hasSystemFeature(PackageManager.FEATURE_TELEPHONY);
+            if (!isMobileData) {
+                PowerWidgetUtil.BUTTONS.remove(PowerWidgetUtil.BUTTON_MOBILEDATA);
+                PowerWidgetUtil.BUTTONS.remove(PowerWidgetUtil.BUTTON_NETWORKMODE);
+                prefButtonsModes.removePreference(mNetworkMode);
             }
 
             // fill that checkbox map!
