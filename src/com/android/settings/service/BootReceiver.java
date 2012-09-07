05adb506ffa55e75ff2e95189ba8eac8bf419c8b
Codenamedroid <bemegadroid@gmail.com>
2012-09-07 10:14:52 -0500

    add kms to the new boot reciver

--- src/com/android/settings/service/BootReceiver.java
+++ src/com/android/settings/service/BootReceiver.java
@@ -4,12 +4,41 @@ package com.android.settings.service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.SystemProperties;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import com.android.settings.performance.KernelControls;
+import com.android.settings.Utils;
 
 public class BootReceiver extends BroadcastReceiver {
+    
+    private static final String TAG = "BootReceiver";
+
+    private static final String KSM_SETTINGS_PROP = "sys.ksm.restored";
 
     @Override
     public void onReceive(Context context, Intent intent) {
         context.startService(new Intent(context, BootService.class));
+
+        if (Utils.fileExists(KernelControls.KSM_RUN_FILE)) {
+            if (SystemProperties.getBoolean(KSM_SETTINGS_PROP, false) == false
+                && intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+                SystemProperties.set(KSM_SETTINGS_PROP, "true");
+                configureKSM(context);
+            } else {
+                SystemProperties.set(KSM_SETTINGS_PROP, "false");
+            }
+        }
     }
 
+    private void configureKSM(Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        
+        boolean ksm = prefs.getBoolean(KernelControls.KSM_PREF, false);
+        
+        Utils.fileWriteOneLine(KernelControls.KSM_RUN_FILE, ksm ? "1" : "0");
+        Log.d(TAG, "KSM settings restored.");
+    }
 }
